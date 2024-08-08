4ceeb37e967f0d4d70a206e04e455010dcb38b9e
sheikhug <sheikhug@users.noreply.github.com>
2024-08-08 16:49:34 +1000

    corrected errors

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group_rule"  "blog_https_in" {
 
 
 resource "aws_security_group_rule"  "blog_http_out" {
-  type        = "engress"
+  type        = "egress"
   from_port   = 0
   to_port    = 0
   protocol    = -1
