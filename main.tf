cbeaa7f46e32bf4f211f064b84b85ed001d6c470
Arammaly <arammaly@gmail.com>
2024-08-24 15:31:42 -0400

    Commi Khmer_web

--- main.tf
+++ main.tf
@@ -18,17 +18,17 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "Khmer_web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.Khmer_web.id]
 
   tags = {
     Name = "Khmer_Pride"
   }
 }
 
-resource "aws_security_group" "blog" {
+resource "aws_security_group" "Khmer_web" {
   name = "blog"
   tags = {
     Terraform = "true"
@@ -36,32 +36,32 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "Khmer_web_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.Khmer_web.id
 }
 
 
-resource "aws_security_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "Khmer_web_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.Khmer_web.id
 }
 
 
-resource "aws_security_group_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "Khmer_web_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.Khmer_web.id
 }
 
