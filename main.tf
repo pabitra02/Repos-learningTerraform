693b8bc826a9f49b4e60cfcdf9015b4b2abfc656
Arammaly <arammaly@gmail.com>
2024-08-24 18:42:34 -0400

    Revert

--- main.tf
+++ main.tf
@@ -18,52 +18,50 @@ data "aws_vpc" "default" {
   default = true
 }
 
-module "Khmer_web_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  # private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  # enable_nat_gateway = true
-  # enable_vpn_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-resource "aws_instance" "Khmer_web" {
+resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [module.Khmer_web_SG.security_group_id]
-
-  subnet_id = module.Khmer_web_vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
-    Name = "Khmer_Pride"
+    Name = "Learning Terraform"
   }
 }
 
-module "Khmer_web_SG" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
+resource "aws_security_group" "blog" {
+  name = "blog"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
 
- # vpc_id              = data.aws_vpc.default.id
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
 
-  vpc_id              = module.Khmer_web_vpc.vpc_id
-  name    = "Khmer-web_noude-sec"
 
-  ingress_rules        = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
 
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
 
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
 
