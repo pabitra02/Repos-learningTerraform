60f50e482d1407fcf3eedb2d3838e69c5e829441
Arammaly <arammaly@gmail.com>
2024-08-24 18:50:34 -0400

    Recreate Dev Enviorment

--- main.tf
+++ main.tf
@@ -18,50 +18,52 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "Khmer_web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  # private_subnets   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "Khmer_web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.Khmer_web.id]
+  vpc_security_group_ids = [module.Khmer_web_SG.security_group_id]
+
+  subnet_id = module.Khmer_web_vpc.public_subnets[0]
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "Khmer_Pride"
   }
 }
 
-resource "aws_security_group" "Khmer_web" {
-  name = "Khmer_web"
-  tags = {
-    Terraform = "true"
-  }
-  vpc_id = data.aws_vpc.default.id
-}
+module "Khmer_web_SG" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
 
-resource "aws_security_group_rule" "Khmer_web_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.Khmer_web.id
-}
+ # vpc_id              = data.aws_vpc.default.id
 
+  vpc_id              = module.Khmer_web_vpc.vpc_id
+  name    = "Khmer-web_noude-sec"
 
-resource "aws_security_group_rule" "Khmer_web_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.Khmer_web.id
-}
+  ingress_rules        = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 
-resource "aws_security_group_rule" "Khmer_web_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.Khmer_web.id
 }
 
