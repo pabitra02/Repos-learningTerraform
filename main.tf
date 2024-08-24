78d6f3195afa2fc402011c4ae8d6f15031f0bf05
Arammaly <arammaly@gmail.com>
2024-08-24 15:27:42 -0400

    change02

--- main.tf
+++ main.tf
@@ -15,60 +15,53 @@ data "aws_ami" "app_ami" {
 }
 
 data "aws_vpc" "default" {
-    default  = true
+  default = true
+}
 
-} 
-
-resource "aws_instance" "Khmer_web" {
-  ami                       = data.aws_ami.app_ami.id
-  instance_type             = var.instance_type
-  vpc_security_group_ids    = [aws_security_group_khmer_web.id]
-
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Khmer_Pride"
   }
 }
 
-# Security Group
-
-resource "aws_security_group" "Khmer_web" {
-  name            = "Khmer_web"
-  description     = "Allow http and https in. Allow everything out"
-
-  vpc_id          = data.aws_vpc.default.id
-
-}   
-
-resource "aws_security_group_rule" "Khmer_web_http_in" {
-  type                = "ingress"
-  from_port           = 80
-  to_port             = 80
-  protocol            = "tcp"
-  cidr_blocks         = ["0.0.0.0/0"]
-  
-  security_group_id   = aws_security_group.Khmer_web.id
-
+resource "aws_security_group" "blog" {
+  name = "blog"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "Khmer_web_https_in" {
-  type                = "ingress"
-  from_port           = 443
-  to_port             = 443
-  protocol            = "tcp"
-  cidr_blocks         = ["0.0.0.0/0"]
-  
-  security_group_id   = aws_security_group.Khmer_web.id
-
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "Khmer_web_everything_out" {
-  type                = "egress"
-  from_port           = 0
-  to_port             = 0
-  protocol            = "-1"
-  cidr_blocks         = ["0.0.0.0/0"]
-  
-  security_group_id   = aws_security_group.Khmer_web.id
 
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
+
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+
