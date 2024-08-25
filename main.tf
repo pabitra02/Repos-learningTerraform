1c6e400ad10b6286fd9a6aaf197fee53a51f613d
Arammaly <arammaly@gmail.com>
2024-08-25 17:12:24 -0400

    comm

--- main.tf
+++ main.tf
@@ -18,22 +18,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-module "Khmer_web_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "Khmer_Web_Dev"
-  cidr = "10.0.0.0/16"
-
-  azs                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
-  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform 	= "true"
-    Environment = "Khmer_Web_Dev"
-  }
-}
-
 resource "aws_instance" "Khmer_web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
@@ -41,39 +25,6 @@ resource "aws_instance" "Khmer_web" {
 
   tags = {
     Name = "Khmer_web"
-  }
-}
-module "Khmer_web_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name = "Khmer_web-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = module.Khmer_web_vpc.vpc_id
-  subnets            = module.Khmer_web_vpc.public_subnets
-  security_groups    = module.Khmer_web_sg.security_group_id
-
-  target_groups = [
-    {
-      name_prefix      = "Khmer_web-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Environment = "dev"
   }
 }
 
@@ -88,3 +39,7 @@ module "Khmer_web_sg" {
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
+
+
+
