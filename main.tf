57f70d4d8170ce1343e07739e6071498d495a919
Arammaly <arammaly@gmail.com>
2024-08-25 15:27:56 -0400

    Creating Load balancer

--- main.tf
+++ main.tf
@@ -28,14 +28,53 @@ resource "aws_instance" "Khmer_web" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "Khmer_web-alb"
+  vpc_id  = module.Khmer_web_vpc.vpc_id
+  subnets = module.Khmer_web_vpc.publick_subnets
+  security_groups = module.Khmer_web_sg.security_group_id
+ 
+  }
+
+listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    
+  }
+    
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "Khmer_web"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.Khmer_web.id
+    }
+  }
+
+  tags = {
+    Environment = "Khmer_web_Dev"
+    Project     = "Using terraform to create this Environment"
+  }
+}
+
 module "Khmer_web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "Khmer_web"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id              = data.aws_vpc.default.id
+  name                = "Khmer_web"
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
