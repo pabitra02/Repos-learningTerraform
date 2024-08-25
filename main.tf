8ed5206a6e623cdf2c7d432153e647e1a74afffb
Arammaly <arammaly@gmail.com>
2024-08-25 16:18:45 -0400

    test commit

--- main.tf
+++ main.tf
@@ -14,23 +14,18 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 module "Khmer_web_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "Khmer_Web_Dev"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
-  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform 	= "true"
-    Environment = "Khmer_Web_Dev"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
@@ -64,35 +59,18 @@ module "Khmer_web_alb" {
   ]
 
   tags = {
-    Environment = "Khmer_web-dev"
-  }
-}
-
-resource "aws_instance" "Khmer_web" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  
-  vpc_security_group_ids = [module.Khmer_web_SG.security_group_id]
-
-  subnet_id				= module.Khmer_web_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Khmer_web"
+    Environment = "dev"
   }
 }
 
 module "Khmer_web_SG" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
+  version = "4.13.0"
 
-
-  vpc_id            = module.Khmer_web_vpc.vpc_id
-  name    			= "Khmer-web-SG"
-
-  ingress_rules        	= ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks 	= ["0.0.0.0/0"]
-
-  egress_rules        	= ["all-all"]
-  egress_cidr_blocks 	= ["0.0.0.0/0"]
-
+  vpc_id  = module.Khmer_web_vpc.vpc_id
+  name    = "Khmer_web"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
