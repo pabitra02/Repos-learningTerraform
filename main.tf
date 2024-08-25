cb707e252b982a4cdc4ecaf765a690d3ea5976f0
Arammaly <arammaly@gmail.com>
2024-08-25 15:31:31 -0400

    Sunday82524 dev lab

--- main.tf
+++ main.tf
@@ -18,63 +18,48 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "Khmer_web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Khmer_Web_Dev"
+  cidr = "10.0.0.0/16"
+
+  azs                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform 	= "true"
+    Environment = "Khmer_Web_Dev"
+  }
+}
+
 resource "aws_instance" "Khmer_web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [module.Khmer_web_sg.security_group_id]
+  
+  vpc_security_group_ids = [module.Khmer_web_SG.security_group_id]
+
+  subnet_id				= module.Khmer_web_vpc.public_subnets[0]
 
   tags = {
     Name = "Khmer_web"
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "Khmer_web-alb"
-  vpc_id  = module.Khmer_web_vpc.vpc_id
-  subnets = module.Khmer_web_vpc.publick_subnets
-  security_groups = module.Khmer_web_sg.security_group_id
- 
-  }
-
-listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    
-  }
-    
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "Khmer_web"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.Khmer_web.id
-    }
-  }
-
-  tags = {
-    Environment = "Khmer_web_Dev"
-    Project     = "Using terraform to create this Environment"
-  }
-}
-
-module "Khmer_web_sg" {
+module "Khmer_web_SG" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.1.2"
 
-  vpc_id              = data.aws_vpc.default.id
-  name                = "Khmer_web"
-  ingress_rules       = ["https-443-tcp","http-80-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
+
+  vpc_id            = module.Khmer_web_vpc.vpc_id
+  name    			= "Khmer-web-SG"
+
+  ingress_rules        	= ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks 	= ["0.0.0.0/0"]
+
+  egress_rules        	= ["all-all"]
+  egress_cidr_blocks 	= ["0.0.0.0/0"]
+
 }
+
