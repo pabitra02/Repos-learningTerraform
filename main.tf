3aec8df56da91b4718d187add4926a870dd64eea
Arammaly <arammaly@gmail.com>
2024-08-27 13:32:22 -0400

    Creating Dev Env 1

--- main.tf
+++ main.tf
@@ -18,28 +18,48 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "Khmer_web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Khmer_dev"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags          = {
+    Terraform   = "true"
+    Environment = "Khmer_dev"
+  }
+}
+
 resource "aws_instance" "Khmer_web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.Khmer_web_sg.security_group_id]
 
-  tags = {
-    Name = "Khmer_web"
+  subnet_id = module.Khmer_web_vpc.public_subnets[0]
+
+  tags    = {
+    Name  = "Khmer_Pride"
   }
 }
 
 module "Khmer_web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.1.2"
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "Khmer_web_acl"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+ # vpc_id               = data.aws_vpc.default.id
+
+  vpc_id                = module.Khmer_web_vpc.vpc_id
+  name    = "Khmer-web_noude-sec"
+
+  ingress_rules         = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks   = ["0.0.0.0/0"]
+
+  egress_rules          = ["all-all"]
+  egress_cidr_blocks    = ["0.0.0.0/0"]
+
 }
-
-
-
 
