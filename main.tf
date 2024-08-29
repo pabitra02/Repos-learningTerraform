5047d1206656450f7f75c6dcdea2297d32d10480
Arammaly <arammaly@gmail.com>
2024-08-29 10:01:11 -0400

    Refactor reuse of variable

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.name] 
 }
 
 data "aws_vpc" "default" {
@@ -21,12 +21,12 @@ data "aws_vpc" "default" {
 module "Khmer_web_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "Khmer_dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}${var.environment.network_prefix}.0.0/16"
 
   azs                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
-  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets      = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
@@ -38,10 +38,10 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "Khmer_web"
+  name = var.environment.name
 
-  min_size            = 1
-  max_size            = 1
+  min_size            = var.asg_min_size
+  max_size            = var.asg_max_size
   
   vpc_zone_identifier = module.Khmer_web_vpc.public_subnets
   target_group_arns   = module.Khmer-web_alb.target_group_arns
@@ -54,7 +54,7 @@ module "Khmer-web_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "Khmer-web-alb"
+  name = "${var.environment.name}-alb"
 
   load_balancer_type = "application"
 
@@ -64,7 +64,7 @@ module "Khmer-web_alb" {
 
 target_groups = [
     {
-      name_prefix      = "web"
+      name_prefix      = "${var.environment.name}web"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -88,10 +88,8 @@ module "Khmer_web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
 
- # vpc_id               = data.aws_vpc.default.id
-
   vpc_id                = module.Khmer_web_vpc.vpc_id
-  name    = "Khmer-web_SG"
+  name    = "${var.environment.name}_SG"
 
   ingress_rules         = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks   = ["0.0.0.0/0"]
