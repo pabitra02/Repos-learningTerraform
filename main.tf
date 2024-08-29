12555401c6b9118f7d85682fa40a3e4ef2c2b3a4
Arammaly <arammaly@gmail.com>
2024-08-29 09:46:47 -0400

    Virable Dev Env 1

--- main.tf
+++ main.tf
@@ -15,27 +15,88 @@ data "aws_ami" "app_ami" {
 }
 
 data "aws_vpc" "default" {
-  default = true
+  default   = true
 }
 
-resource "aws_instance" "Khmer_web" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.Khmer_web_sg.security_group_id]
+module "Khmer_web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Khmer_dev"
+  cidr = "10.0.0.0/16"
+
+  azs                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Name = "Khmer_web"
+    Terraform = "true"
+    Environment = "Khmer_dev"
+  }
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "Khmer_web"
+
+  min_size            = 1
+  max_size            = 1
+  
+  vpc_zone_identifier = module.Khmer_web_vpc.public_subnets
+  target_group_arns   = module.Khmer-web_alb.target_group_arns
+  security_groups     = [module.Khmer_web_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+
+module "Khmer-web_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "Khmer-web-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.Khmer_web_vpc.vpc_id
+  subnets            = module.Khmer_web_vpc.public_subnets
+  security_groups    = [module.Khmer_web_sg.security_group_id]
+
+target_groups = [
+    {
+      name_prefix      = "web"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags          = {
+    Environment = "dev"
   }
 }
 
 module "Khmer_web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.1.2"
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "Khmer_web_acl"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+ # vpc_id               = data.aws_vpc.default.id
+
+  vpc_id                = module.Khmer_web_vpc.vpc_id
+  name    = "Khmer-web_SG"
+
+  ingress_rules         = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks   = ["0.0.0.0/0"]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+
 }
