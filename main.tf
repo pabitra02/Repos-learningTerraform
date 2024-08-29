c58c496ee2082045b5baf808b0ea2ef486e31822
Arammaly <arammaly@gmail.com>
2024-08-29 10:19:57 -0400

    typo 4

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "Khmer-web_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "${var.environment.name}-alb"
+  name = "${var.environment.name}alb"
 
   load_balancer_type = "application"
 
