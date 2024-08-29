4db9007c64b910b1565a3afd1a5463e5b191d1f3
Arammaly <arammaly@gmail.com>
2024-08-29 11:56:27 -0400

    try again 2

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "Khmer-alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "${var.environment.name}_1"
+  name = "${var.environment.name}"
 
   load_balancer_type = "application"
 
