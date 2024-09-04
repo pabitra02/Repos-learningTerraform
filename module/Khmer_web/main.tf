77bda754bb0931b9e0687a9923065b64f95615fb
Arammaly <arammaly@gmail.com>
2024-09-04 15:57:41 -0400

    try again 2

--- module/Khmer_web/main.tf
+++ module/Khmer_web/main.tf
@@ -68,14 +68,6 @@ module "Khmer-web_alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
   tags = {
     Environment = "dev"
   }
