1ed8dfab4b8b21956ab6caa3ea3bf7a502b01712
RedGlare-05 <shreyasj.ae20@gmail.com>
2024-11-03 18:21:34 +0530

    AWS_SG

--- main.tf
+++ main.tf
@@ -14,11 +14,55 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+
+data "aws_VPC" "default"{
+  default = true 
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_gorup_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog_sg"{
+  name        = "blog"
+  description = "Web traffic https http in, and allow everything"
+
+  vpc_id = data.aws_VPC.default.vpc_id
+}
+
+resource"aws_security_group_in" "blog_http_in"{
+  type        = "ingress"
+  from_port   = 80
+  in_port     = 80
+  protocol    = "tcp"
+  cidar_block = ["0.0.0.0/0"]
+
+  security_group_id = "aws_security_group.blog.id" 
+}
+
+resource"aws_security_group_in" "blog_https_in"{
+  type        = "ingress"
+  from_port   = 443
+  in_port     = 443
+  protocol    = "tcp"
+  cidar_block = ["0.0.0.0/0"]
+
+  security_group_id = "aws_security_group.blog.id" 
+}
+
+resource"aws_security_group_in" "blog_everything_out"{
+  type        = "egress"
+  from_port   = 0
+  in_port     = 0
+  protocol    = "-1"
+  cidar_block = ["0.0.0.0/0"]
+
+  security_group_id = "aws_security_group.blog.id" 
 }
