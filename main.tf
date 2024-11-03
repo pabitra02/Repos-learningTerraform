9b2e65ebaf67a915a3ad93295be6c39fdae80258
RedGlare-05 <shreyasj.ae20@gmail.com>
2024-11-03 18:27:01 +0530

    SG_UPDATED V2

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource"aws_security_group_in" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidar_block = ["0.0.0.0/0"]
+  cidr_block = ["0.0.0.0/0"]
 
   security_group_id = "aws_security_group.blog.id" 
 }
@@ -52,7 +52,7 @@ resource"aws_security_group_in" "blog_https_in" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidar_block = ["0.0.0.0/0"]
+  cidr_block = ["0.0.0.0/0"]
 
   security_group_id = "aws_security_group.blog.id" 
 }
@@ -62,7 +62,7 @@ resource"aws_security_group_in" "blog_everything_out"{
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidar_block = ["0.0.0.0/0"]
+  cidr_block = ["0.0.0.0/0"]
 
   security_group_id = "aws_security_group.blog.id" 
 }
