201db9e235fbd7a897813e74aaccc0417263e22d
RedGlare-05 <shreyasj.ae20@gmail.com>
2024-11-03 18:37:14 +0530

    sg_v8

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "blog"{
   name        = "blog"
   description = "Web traffic https http in, and allow everything"
 
-  vpc_id = data.aws_vpc.default.vpc_id
+  vpc_id = data.aws_vpc.default.aws_vpc.id
 }
 
 resource "aws_security_group_in" "blog_http_in" {
